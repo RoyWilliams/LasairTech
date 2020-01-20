b37e876cc1965f89e7fc5f08b5d140c824c8013a
Roy Williams <roy@roe.ac.uk>
2020-01-20 14:46:08 +0000

    update

--- database_tests/ansible/mysql_setup.sql
+++ database_tests/ansible/mysql_setup.sql
@@ -2,7 +2,7 @@ SET GLOBAL validate_password_length = 6;
 SET GLOBAL validate_password_special_char_count = 0;
 SET GLOBAL validate_password_mixed_case_count = 0;
 SET GLOBAL validate_password_policy = 'LOW';
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'EH107HWpostcode';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'rootpassword';
 FLUSH PRIVILEGES;
 CREATE USER 'ztf'@'localhost' IDENTIFIED BY 'password';
 CREATE USER 'ztf'@'%' IDENTIFIED BY 'password';
