00935feccdd55de5ad9d1f62f45dcdfa3e5bafe2
Roy Williams <roy@roe.ac.uk>
2020-01-30 14:08:37 +0000

    update

--- database_tests/runit.bash
+++ database_tests/runit.bash
@@ -2,5 +2,5 @@ dir='LasairTech/database_tests'
 archive='192.168.140.23'
 python3 start_again.py
 python3 driver_local.py 1000000 10000
-time scp out.txt $archive:scratch
+time scp /home/ubuntu/scratch/out.txt $archive:scratch
 time ssh         $archive "mysql --user=ztf --password=123password < $dir/load.sql"
