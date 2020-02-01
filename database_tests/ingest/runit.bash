fc1b896d8e4c97052c4631f5b20cb94682bc4265
Roy Williams <roy@roe.ac.uk>
2020-02-01 09:20:54 +0000

    update

---
+++ database_tests/ingest/runit.bash
@@ -0,0 +1,6 @@
+python3 ingestStreamThreaded.py \
+--maxalert 10000 \
+--nthread 1 \
+--group LASAIR-DEV2 \
+--host 192.41.108.22 \
+--topic ztf_20200131_programid1
---
+++ database_tests/ingest/settings.py
@@ -0,0 +1,8 @@
+DB_HOST_LOCAL = 'localhost'
+DB_HOST_REMOTE = '192.168.140.23'
+DB_USER_WRITE = 'ztf'
+DB_PASS_WRITE = '123password'
+DB_DATABASE   = 'ztf'
+
+KAFKA_TIMEOUT = 60
+
