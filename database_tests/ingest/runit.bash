7c6af67fa10d3d0395bd250f75c76f6003c0bcd5
Roy Williams <roy@roe.ac.uk>
2020-02-01 10:37:34 +0000

    update

--- database_tests/ingest/runit.bash
+++ database_tests/ingest/runit.bash
@@ -1,6 +1,13 @@
+python3 refresh.py
+
 python3 ingestStreamThreaded.py \
 --maxalert 10000 \
 --nthread 1 \
 --group LASAIR-DEV2 \
 --host 192.41.108.22 \
 --topic ztf_20200131_programid1
+
+mysqldbexport --server=ztf:123password@localhost:3306 --format=csv ztf --export=data > ~/scratch/out.txt
+
+time scp /home/ubuntu/scratch/out.txt 192.168.140.23:scratch
+time ssh 192.168.140.23 "mysql --user=ztf --password=123password < LasairTech/database_tests/ingest/load.sql"
--- database_tests/toys/runit.bash
+++ database_tests/toys/runit.bash
@@ -1,6 +1,6 @@
-dir='LasairTech/database_tests'
+dir='LasairTech/database_tests/toys'
 archive='192.168.140.23'
 python3 start_again.py
-python3 driver_local.py 1000000 10000
+python3 driver_local.py 1000000 100000
 time scp /home/ubuntu/scratch/out.txt $archive:scratch
 time ssh         $archive "mysql --user=ztf --password=123password < $dir/load.sql"
