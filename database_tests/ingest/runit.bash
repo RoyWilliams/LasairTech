4f6ea0f80336d5fe1a8def077a724fef0961c31b
Roy Williams <roy@roe.ac.uk>
2020-02-05 14:47:23 +0000

    update

--- database_tests/ingest/runit.bash
+++ database_tests/ingest/runit.bash
@@ -12,6 +12,6 @@ mysql --user=ztf --database=ztf --password=123password < output_csv.sql
 mv /var/lib/mysql-files/out.txt /home/ubuntu/scratch
 
 out=`hostname`.txt
-time scp /home/ubuntu/scratch/out.txt 192.168.140.23:scratch/$out
+time scp /home/ubuntu/scratch/out.txt 192.168.140.47:scratch/$out
 
-time ssh 192.168.140.23 "python3 /home/ubuntu/LasairTech/database_tests/ingest/in.py $out"
+time ssh 192.168.140.47 "python3 /home/ubuntu/LasairTech/database_tests/ingest/in.py $out"
--- database_tests/ingest/settings.py
+++ database_tests/ingest/settings.py
@@ -1,5 +1,5 @@
 DB_HOST_LOCAL = 'localhost'
-DB_HOST_REMOTE = '192.168.140.23'
+DB_HOST_REMOTE = '192.168.140.47'
 DB_USER_WRITE = 'ztf'
 DB_PASS_WRITE = '123password'
 DB_DATABASE   = 'ztf'
