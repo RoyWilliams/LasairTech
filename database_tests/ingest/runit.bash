156c2738a38649f0cca7cdedcf1c6529bc7e1e62
Roy Williams <roy@roe.ac.uk>
2020-02-07 17:08:14 +0000

    update

--- database_tests/ingest/runit.bash
+++ database_tests/ingest/runit.bash
@@ -1,3 +1,6 @@
+for value in {1..2000000}
+do
+
 python3 refresh.py
 
 python3 ingestStreamThreaded.py \
@@ -11,7 +14,14 @@ mysql --user=ztf --database=ztf --password=123password < output_csv.sql
 
 mv /var/lib/mysql-files/out.txt /home/ubuntu/scratch
 
+if [ ! -s /home/ubuntu/scratch/out.txt ]
+then
+    echo 'No more alerts -- exiting'
+    exit 0
+fi
+
 out=`hostname`.txt
 time scp /home/ubuntu/scratch/out.txt 192.168.140.47:scratch/$out
 
 time ssh 192.168.140.47 "python3 /home/ubuntu/LasairTech/database_tests/ingest/in.py $out"
+done
