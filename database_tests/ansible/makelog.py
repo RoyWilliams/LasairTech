9f9bc22ada03705db67aff3af7240e911829061b
Roy Williams <roydavidwilliams@gmail.com>
2020-02-20 10:19:51 +0000

    update

--- database_tests/ansible/makelog.py
+++ database_tests/ansible/makelog.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 alldata = json.loads(open('logs.json').read())
 n = len(alldata.keys())
-fig, axes = plt.subplots(1, n)
+fig, axes = plt.subplots(2, int(n/2))
 j = 0
 for (host, data) in alldata.items():
     start = []
@@ -16,8 +16,10 @@ for (host, data) in alldata.items():
         start.append (d['start'])
         apm.append(d['alerts']*60.0/d['run'])
         run.append   (d['run'])
-    axes[j].bar(start, apm, run, align='edge')
-    axes[j].set(ylabel='alerts per minute ' + host)
-    axes[j].set_ylim(ymax=400)
+    ax = axes[int(j/2), j%2]
+    ax.bar(start, apm, run, align='edge')
+    ax.set(ylabel='alerts per minute ' + host)
+    ax.set_ylim(ymax=400)
     j += 1
-plt.show()
+#plt.show()
+plt.savefig('logs.png')
